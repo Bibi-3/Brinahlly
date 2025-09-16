a4ffe3551506549b495b40e7e1ea2c2a8e9d0825
Bibi-3 <lopes.silva.gabriele@escola.pr.gov.br>
2025-09-16 16:55:22 -0300

    js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Dropdown do perfil
 function toggleDropdown() {
-  document.getElementById("dropdownMenu").classList.toggle("show");
+  document.getElementById("dropdownMenu").classList.toggle("active");
 }
 
 // Função para mostrar páginas (Login/Cadastro)
